ab2c07d1ffac8643596b0906f17895cba4bbd713
fany-santos8 <estefany_fanysantos@hotmail.com>
2025-03-24 00:06:44 -0300

    first commit

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
    
-    baseURL: 'https://back-forneria77.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
